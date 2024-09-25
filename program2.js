b3eaae7c886389940a1413f3e57e7fe8063f453f
Abhishek Patel <abhidhakad9770@gmail.com>
2024-09-26 01:08:58 +0530

    program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,30 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function romanToInt(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
+    let total = 0;
+    let prevValue = 0;
 
-module.exports={romanToInt}
+    for (let char of s) {
+        const currentValue = romanMap[char];
+
+        if (currentValue > prevValue) {
+            total += currentValue - 2 * prevValue; 
+        } else {
+            total += currentValue;  
+        }
+
+        prevValue = currentValue;  
+    }
+
+    return total;
+}
+
+module.exports = { romanToInt };
